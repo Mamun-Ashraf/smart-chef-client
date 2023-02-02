e66d4b38d585aea6f9b19b707e7b65fbdcd91fc0
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-02-02 23:07:11 +0600

    added favicon and title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is header</h1>
-      <button type="button" className="px-8 py-3 font-semibold rounded dark:bg-gray-100 dark:text-gray-800">Basic</button>
-      <button type="button" className="px-8 py-3 font-semibold border rounded dark:border-gray-100 dark:text-gray-100">Border</button>
-      <button type="button" className="px-8 py-3 font-semibold rounded-full dark:bg-red-500 dark:text-purple-800">Rounded</button>
-      <br />
-      <button type="button" className="px-8 py-3 font-semibold rounded-full bg-yellow-400 text-red-500">Rounded</button>
-      <br />
-      <button type="button" className="px-8 py-3 font-semibold border rounded border-gray-800 text-gray-800">Border</button>
-      <br />
-      <button type="button" className="px-8 py-3 font-semibold rounded bg-blue-500 text-gray-100">Basic</button>
+
     </div>
   );
 }
