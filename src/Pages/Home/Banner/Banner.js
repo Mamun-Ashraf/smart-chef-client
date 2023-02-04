cef42e05aad57cbc3ecab5aaa1574181317e8081
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-02-04 11:01:56 +0600

    added navbar and banner section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Banner.css'
+import pic from "../../../Assets/kitchen-10.jpg";
 
 const Banner = () => {
     return (
-        <div>
-            <h2 className="text-3xl">Banner</h2>
+        <div className='relative'>
+            <div className='banner-img'>
+                <img className='w-full mx-auto my-10' src={pic} alt="" />
+            </div>
+            <div className='absolute  right-12 top-1/4'>
+                <h2 className="text-5xl font-bold text-white">
+                    <span className='text-pink-400'>Cooked With</span>
+                    <br />
+                    <span className='text-rose-200'>Mothers Hand !!!</span>
+                </h2>
+            </div>
+            <br />
+            <p className='absolute right-12 top-1/3 float-right w-1/3 text-white text-xl'>
+                We serve <span className='text-3xl text-green-300'>completely home made</span> foods,
+                <br />
+                made by life long experienced(in cook) mothers/sisters.
+                <br />
+                We have no traditional Restaurant.
+                <br />
+                Only the proffesional and trained up mothers/sisters(housewife) are our cook partner(chef) who made your food with care in their own kitchen.</p>
         </div>
     );
 };
